7b8957b1d5d32d4cb542e7af9408d6a1eddc24b9
Matt Goldman <matt.goldman@outlook.com>
2022-06-18 18:56:13 +1000

    Add project files.

---
+++ ShadowBug/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ShadowBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ShadowBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ShadowBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
